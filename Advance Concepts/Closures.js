800a9310be15cf9572e5d06529cc04997b817085
Adarsh <adarsh800251@gmail.com>
2022-10-22 22:16:33 +0530

    Basic:console & Dom & Advance:Interview ex

--- Advance Concepts/Closures.js
+++ Advance Concepts/Closures.js
@@ -13,6 +13,13 @@ z();  // function invoked which was returned.
 
 // z contains the whole function with its lexical scope.
 
+// Advantages
 
+// They provide data encapsulation.
+// //They allow you to attach variables to an execution context.
+// Variables in closures can help you maintain a state that you can use later.
+// They provide data encapsulation.
+// They help remove redundant code.
+// They help maintain modular code.
 
 
---
+++ Html,Css,js/index.js
@@ -0,0 +1,9 @@
+// Window Object
+
+console.log(window) // Global object
+// window.alert("Man")
+
+// document object used to manipulate elements of our page
+
+document.body.style.background="red"
+
