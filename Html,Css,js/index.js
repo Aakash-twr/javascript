57fbf0b7d64c0c837a45e3f0d1e2b843ea8cbaa2
Adarsh <adarsh800251@gmail.com>
2022-10-24 03:45:21 +0530

    basic: practice

--- Html,Css,js/index.js
+++ Html,Css,js/index.js
@@ -5,5 +5,13 @@ console.log(window) // Global object
 
 // document object used to manipulate elements of our page
 
-document.body.style.background="red"
+document.body.style.background="gray"
 
+let a = prompt("Enter your age");
+let age = Number.parseInt(a);
+
+if(age>18){
+    document.body.style.background='yellow'
+}else{
+    alert("You cannot drive");
+}
