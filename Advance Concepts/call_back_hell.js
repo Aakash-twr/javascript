cfdaa2072daaff90a7ea873dc8106f3c9646b937
Adarsh <adarsh800251@gmail.com>
2023-01-29 01:59:57 +0530

    call_back_hell updated

--- Advance Concepts/call_back_hell.js
+++ Advance Concepts/call_back_hell.js
@@ -4,5 +4,25 @@ const cart = ['shoes','pants','kurt'];
 
 // Callback hell - Callback functions nested one above another creates a pyramid like structure.
 
+
+function thing1(callback){
+    callback();
+}
+function thing2(callback){
+    callback();
+}
+function thing3(callback){
+    callback();
+}
+
+
+thing1(()=>{
+    thing2(()=>{
+        thing3(()=>{
+            console.log('Done')
+        })
+    })
+})
+
 // Inversion of Control - Problem occur when we deal with multiple callbacks and we lose control over our program.
 
